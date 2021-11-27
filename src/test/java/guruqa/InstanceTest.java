2a42b3957393ecbecaaadaa15bb1ed9ac700b45f
evgeniashennikova <evgeniashenikova@gmail.com>
2021-11-27 20:16:15 +0300

    Добавлен первый тест

---
+++ src/test/java/guruqa/InstanceTest.java
@@ -0,0 +1,23 @@
+package guruqa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class InstanceTest {
+
+    @Test
+    void simpleTest() {
+
+        String quest = "2+6";
+        String expectedResult = "8";
+
+        open("https://www.google.ru/");
+        $("[name='q']").setValue(quest);
+        $("[name='btnK']").click();
+        String actualResult = $("#cwos").getText();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+}
